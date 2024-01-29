86d42ad5a3211d9a60d821ed3a3f5da7d7fff4a1
kalyani <n.chandra.kalyani@gmail.com>
2024-01-29 17:34:49 +0530

    intial commit msg for str search

---
+++ string_search.py
@@ -0,0 +1,25 @@
+import os
+text = input("input string")
+path = input('input path:')
+def get_files(path):
+    f = 0
+    os.chdir(path)
+    files = os.listdir()
+    for filename in files:
+        abspath = os.path.abspath(filename)
+        if os.path.isdir(abspath):
+            get_files(abspath)
+        if os.path.isfile(abspath):
+            f = open(filename,'r')
+            if text in f.read():
+                f=1
+                print(text,'Found in')
+                final_path = os.path.abspath(filename)
+                print(final_path)
+                return True
+    if f == 0:
+        print(text,'Not Found')
+        return False
+    
+
+get_files(path)
